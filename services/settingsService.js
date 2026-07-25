bc5f90691bc97fce1cb879f19e3101f704e8dec0
atuning120 <cristian.ignacio.nunez@gmail.com>
2026-07-25 19:58:09 -0400

    editar correos, direccion, telefono

--- services/settingsService.js
+++ services/settingsService.js
@@ -14,13 +14,29 @@ async function getSettings() {
   if (!settings) {
     return {
       storeAddressName: process.env.VITE_STORE_ADDRESS || 'Maipú 942 Este, San Juan, Argentina',
-      storeAddressMapUrl: 'https://maps.google.com/maps?q=-31.5402377,-68.5173167&hl=es&z=16&output=embed'
+      storeAddressMapUrl: 'https://maps.google.com/maps?q=-31.5402377,-68.5173167&hl=es&z=16&output=embed',
+      emailCorporativoTitle: 'Email Corporativo',
+      emailCorporativo: process.env.VITE_EMAIL || 'ventas@ledclean.ar',
+      emailConsultasTitle: 'Consultas y Ayuda',
+      emailConsultas: process.env.VITE_CONSULTAS_EMAIL || 'consultas@ledclean.ar',
+      whatsappSoporteTitle: 'WhatsApp Soporte',
+      whatsappSoporte: process.env.VITE_WHATSAPP_PHONE || '',
+      whatsappComercialTitle: 'WhatsApp Comercial',
+      whatsappComercial: process.env.VITE_WHATSAPP_SECOND || ''
     };
   }
 
   return {
     storeAddressName: settings.storeAddressName || process.env.VITE_STORE_ADDRESS || 'Maipú 942 Este, San Juan, Argentina',
-    storeAddressMapUrl: settings.storeAddressMapUrl || 'https://maps.google.com/maps?q=-31.5402377,-68.5173167&hl=es&z=16&output=embed'
+    storeAddressMapUrl: settings.storeAddressMapUrl || 'https://maps.google.com/maps?q=-31.5402377,-68.5173167&hl=es&z=16&output=embed',
+    emailCorporativoTitle: settings.emailCorporativoTitle || 'Email Corporativo',
+    emailCorporativo: settings.emailCorporativo || process.env.VITE_EMAIL || 'ventas@ledclean.ar',
+    emailConsultasTitle: settings.emailConsultasTitle || 'Consultas y Ayuda',
+    emailConsultas: settings.emailConsultas || process.env.VITE_CONSULTAS_EMAIL || 'consultas@ledclean.ar',
+    whatsappSoporteTitle: settings.whatsappSoporteTitle || 'WhatsApp Soporte',
+    whatsappSoporte: settings.whatsappSoporte || process.env.VITE_WHATSAPP_PHONE || '',
+    whatsappComercialTitle: settings.whatsappComercialTitle || 'WhatsApp Comercial',
+    whatsappComercial: settings.whatsappComercial || process.env.VITE_WHATSAPP_SECOND || ''
   };
 }
 
@@ -31,6 +47,14 @@ async function updateSettings(newSettings) {
     $set: {
       storeAddressName: newSettings.storeAddressName,
       storeAddressMapUrl: newSettings.storeAddressMapUrl,
+      emailCorporativoTitle: newSettings.emailCorporativoTitle,
+      emailCorporativo: newSettings.emailCorporativo,
+      emailConsultasTitle: newSettings.emailConsultasTitle,
+      emailConsultas: newSettings.emailConsultas,
+      whatsappSoporteTitle: newSettings.whatsappSoporteTitle,
+      whatsappSoporte: newSettings.whatsappSoporte,
+      whatsappComercialTitle: newSettings.whatsappComercialTitle,
+      whatsappComercial: newSettings.whatsappComercial,
       updatedAt: new Date()
     }
   };
